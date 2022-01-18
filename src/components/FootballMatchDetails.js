0c86e319d64f462b95eaf00ce2099d7780237a02
dushyanth <kd13@iitbbs.ac.in>
2022-01-18 11:54:31 +0530

    football match details

--- src/components/FootballMatchDetails.js
+++ src/components/FootballMatchDetails.js
@@ -1,30 +1,98 @@
 import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import {fetchMatchDetails} from '../redux'
-import { Typography } from '@mui/material'
+import { fetchMatchDetails } from '../redux'
+import { Typography, Grid } from '@mui/material'
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import { experimentalStyled as styled } from '@mui/material/styles';
 import { connect } from 'react-redux'
-
-const FootballMatchDetails = ({matchDetails,fetchMatchDetails})=>{
-    const {id} = useParams();
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+const FootballMatchDetails = ({ matchDetails, fetchMatchDetails }) => {
+    const { id } = useParams();
     const url = `/api/football-match-details/${id}`
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchMatchDetails(url)
-    },[])
+    }, [])
 
-    console.log("match details",matchDetails)
-    return (
-        <Typography> Dushyanth </Typography>
+    console.log("match details", matchDetails)
+    return (matchDetails.loading ? "Loading" : ( 
+        <Grid container spacing={2} minWidth={300} padding={20}>
+            <Grid item xs={12}>
+                <Item>{matchDetails.matchDetails.tournamentName}</Item>
+            </Grid>
+            <Grid item xs={12}>
+                <Item>{matchDetails.matchDetails.date} &#8226; {matchDetails.matchDetails.footballTeamScores[0].name} VS {matchDetails.matchDetails.footballTeamScores[1].name} &#8226; match #{matchDetails.matchDetails.number}</Item>
+            </Grid>
+            <Grid item xs={6}>
+                    <Item>
+                    <Typography variant ="h5">{matchDetails.matchDetails.footballTeamScores[0].name}</Typography>
+                    {
+                        matchDetails.matchDetails.footballTeamScores[0].footballPlayerScores.map((score,i) =>{
+                            return (
+                                <React.Fragment key={i}>
+                                <Typography variant="span">
+                                    {score.name} : 
+                                </Typography>
+                                {
+                                    score.goals.map((goal,i)=>{
+                                        return (
+                                            <Typography variant="span" key={i}>
+                                                {goal}
+                                            </Typography>
+                                        )
+                                    })
+                                }
+                                <br/>
+                                </React.Fragment>
+                            )
+                        })
+                    }
+                </Item>
+            </Grid>
+            <Grid item xs={6}>
+                <Item>
+                    <Typography variant ="h5">{matchDetails.matchDetails.footballTeamScores[1].name}</Typography>
+                    {
+                        matchDetails.matchDetails.footballTeamScores[1].footballPlayerScores.map((score,i) =>{
+                            return (
+                                <React.Fragment key={i}>
+                                <Typography variant="span">
+                                    {score.name} : 
+                                </Typography>
+                                {
+                                    score.goals.map((goal,i)=>{
+                                        return (
+                                            <Typography variant="span" key={i}>
+                                                {goal+" "}
+                                            </Typography>
+                                        )
+                                    })
+                                }
+                                <br/>
+                                </React.Fragment>
+                            )
+                        })
+                    }
+                </Item>
+                
+            </Grid>
+        </Grid>)
     )
 }
-const mapStateToProps = (state)=>{
+const mapStateToProps = (state) => {
     return {
-        matchDetails : state.footballMatchDetails
+        matchDetails: state.footballMatchDetails
     }
 }
-const mapDispatchToProps = (dispatch)=>{
+const mapDispatchToProps = (dispatch) => {
     return {
-        fetchMatchDetails : (url)=> dispatch(fetchMatchDetails(url))
+        fetchMatchDetails: (url) => dispatch(fetchMatchDetails(url))
     }
 }
-export default connect(mapStateToProps,mapDispatchToProps)(FootballMatchDetails)
+export default connect(mapStateToProps, mapDispatchToProps)(FootballMatchDetails)
--- src/components/FootballMatches.js
+++ src/components/FootballMatches.js
@@ -2,7 +2,7 @@ import { Typography, ListItem, ListItemText, Divider, List } from "@mui/material
 import React, { useEffect } from "react"
 import { connect } from 'react-redux'
 import { Link } from "react-router-dom"
-import {fetchMatches} from "../redux/"
+import { fetchMatches } from "../redux/"
 
 const FootballMatches = ({ matchesData, fetchMatches }) => {
     useEffect(() => {
@@ -16,37 +16,28 @@ const FootballMatches = ({ matchesData, fetchMatches }) => {
                     matchesData.matches.map(match => (
                         <ListItem alignItems="flex-start" key={match.id}>
                             <ListItemText
-                                primary= {<Typography>{match.date} &#8226; match# {match.number}  &#8226; {match.tournamentName}</Typography>}
-                                 /*{match.date + " &#8226; match #" + match.number + " &#8226;" + match.tournamentName}*/
-                                secondary={
+                                primary={
                                     <React.Fragment>
-                                        <Typography
-                                            sx={{ display: 'inline' }}
-                                            component="span"
-                                            variant="body2"
-                                            color="text.primary"
-                                        >
+                                        <Typography>{match.date} &#8226; match# {match.number}  &#8226; {match.tournamentName}
+                                        </Typography>
+                                        <Typography>
                                             {match.team1Name} - {match.team1Goals}
                                         </Typography>
-                                        <br/>
-                                        <Typography
-                                            sx={{ display: 'inline' }}
-                                            component="span"
-                                            variant="body2"
-                                            color="text.primary"
-                                        >
-                                           {match.team2Name} - {match.team2Goals}
+                                        <Typography>
+                                            {match.team2Name} - {match.team2Goals}
                                         </Typography>
                                         <Link to={`/football-match-details/${match.id}`}>
                                             click-here
                                         </Link>
+                                        <Divider variant="fullWidth" component="p" variant="fullWidth" />
                                     </React.Fragment>
                                 }
                             />
+
                         </ListItem>
+
                     ))
                 }
-                <Divider variant="fullWidth" component="li" />
             </List>
         ))
 }
